1158e8cd042205f83daa7df19b49f64ba8ce4a14
keira25 <keira1425@gmail.com>
2018-01-03 00:48:08 +0800

    Fixed the spelling of 'join'

--- psboxsite/settings.py
+++ psboxsite/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.joine(BASE_DIR, 'templates', 'account'),
+            os.path.join(BASE_DIR, 'templates', 'account'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
